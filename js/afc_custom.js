6329ad2ac2832744a265eec6b943e9b8ce71b7d8
mattc321 <mcampbell@ashlandfood.coop>
2017-02-03 17:46:55 -0500

    js for auto reload after idle time

--- js/afc_custom.js
+++ js/afc_custom.js
@@ -3,12 +3,25 @@
  * This file contains all jQuery for the forms used in afc_custom
  */
 
+
+function timerIncrement() {
+
+	idleTime = idleTime + 1;
+	console.log(idleTime);
+	if (idleTime > 2) { // 20 minutes
+		console.log('12 seconds');
+		window.location.reload();
+	}
+}
+var idleTime = 0;
 (function ($) {
 	  Drupal.behaviors.ec_app = {
 		attach: function (context, settings) {
 			
 		$(window).load(function() {
 			console.log('Afc Custom Module loaded');
+
+
 		});
 
 
@@ -22,6 +35,20 @@
 				} else {
 					$(this).addClass('zoomin');
 				}
+			});
+
+			//IDLE COUNTER
+			//Increment the idle time counter every minute.
+			var idleInterval = setInterval(timerIncrement, 6000); // 1 minute 60000
+			console.log(0);
+			//Zero the idle timer on mouse movement.
+			$(this).mousemove(function(e) {
+				idleTime = 0;
+				console.log(1);
+			});
+			$(this).keypress(function(e) {
+				idleTime = 0;
+				console.log(2);
 			});
 
 		}
