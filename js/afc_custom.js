96b5ecd85f18015aca1940aa5e70c04faa0ab920
mattc321 <mcampbell@ashlandfood.coop>
2017-03-02 12:51:28 -0500

    major updates to shoutout

--- js/afc_custom.js
+++ js/afc_custom.js
@@ -35,12 +35,32 @@ var idleTime = 0;
 		//Shoutout page scripts and handlers
 		if ($('#shoutout-page-main-content').length > 0) {
 
+			//details[i].open = false;
+			//$('details.dept-group').click(function() {
+			//	$("details.dept-group").each(function(index) {
+			//		if ($(this).attr('open')) {
+			//			console.log('its true');
+			//			$(this).attr('open', false);
+			//		} else {
+			//			console.log('its not ture');
+			//		}
+            //
+			//	});
+			//	$(this).attr('open', true);
+			//});
+
+			$('#make-shoutout').click(function() {
+				$('#shoutout-popup1').fadeIn(2000);
+
+			});
+
+			//display ajax messages after posting shoutout
 			if ($('#ajax-msg').length > 0) {
 				$('#ajax-msg').addClass('bounce');
 				$('#ajax-msg').delay(4000).fadeOut('slow');
 				window.setTimeout(function(){location.reload()},4100);
 
-				//onclick of huddle news
+				//onclick of ajax response
 				$('#ajax-msg').click(function() {
 					$('#ajax-msg').fadeOut('slow');
 					location.reload();
@@ -51,14 +71,14 @@ var idleTime = 0;
 
 			//
 			$('.shout-buttons').click(function() {
-				console.log(1);
 
 			});
 
 			//onclick of search employees
-			$('#employee-search-icon').click(function() {
+			$('#employee-search-icon').once().click(function() {
 				if ($('#employee-search-field').is(":hidden")) {
 					$('#employee-search-field').fadeIn('slow');
+					$("#employee-search").focus();
 				} else {
 					$('#employee-search-field').fadeOut('slow');
 				}
@@ -77,34 +97,92 @@ var idleTime = 0;
 			//IDLE COUNTER
 			//Increment the idle time counter every minute.
 			var idleInterval = setInterval(timerIncrement, 15000); // 1 minute 60000
-
+			//shout stats search icon button
+			$('#reset').once().click(function() {
+				if ($('div.view-shoutout-comment-list div.view-filters').is(":hidden")) {
+				} else {
+					$('div.view-shoutout-comment-list div.view-filters').fadeOut('slow');
+					$('#reset').remove();
+				}
+			});
 			//shout stats search icon button
 			$('#shout-search-icon').once().click(function() {
 				if ($('div.view-shoutout-comment-list div.view-filters').is(":hidden")) {
 					$('div.view-shoutout-comment-list div.view-filters').fadeIn('slow');
+					$('#edit-field-full-name-value-shout-out').focus();
+					if ($('#reset').length <= 0) {
+						$('.view-shoutout-comment-list').before('<button id="reset">Reset</button>');
+					}
+
 				} else {
 					$('div.view-shoutout-comment-list div.view-filters').fadeOut('slow');
+					$('#reset').remove();
 				}
 
 			});
 
-			//popups
-			$('.popup-shoutout-info').once().click(function() {
-				console.log('popup');
-				$( "#ajax-response" ).after('<div id="popup-shoutout-info" style="display:none;">Enter your employee# so we know who the shoutout is from</div>');
-				$( "#ajax-response" ).after('<div id="popup-shoutout-choose" style="display:none;">Choose a response</div>');
-				$( "#ajax-response" ).after('<div id="popup-shoutout-submit" style="display:none;">Then click submit</div>');
+			//onclick of shoutout comment
+			$('.shoutout-comment-row').click(function(e) {
 
-				$('#popup-shoutout-info').delay(0050).fadeIn('slow');
-				$('#popup-shoutout-info').delay(1700).fadeOut('slow');
+				if ($(this).hasClass('comment-hover')) {
+					//make it small
+					$(this).removeClass('comment-hover');
+					$(".shoutout-comment-row").each(function(index) {
+						$(this).removeClass("gray-out");
+						//$(this).removeClass("bounceIn animated");
+					});
+					$(this).css({position:'inherit'});
+				} else {
+					//make it big
+					//before you make it big make all of them small
+					$(".shoutout-comment-row").each(function(index) {
+						$(this).removeClass("comment-hover");
+						$(this).css({position:'inherit'});
+					});
 
-				$('#popup-shoutout-choose').delay(2200).fadeIn('slow');
-				$('#popup-shoutout-choose').delay(2500).fadeOut('slow');
+					//set position and assign gray out class to everything else
+					$(this).css({top: e.pageY + 'px', left: e.pageX-340 + 'px', position:'absolute'});
+					$(".shoutout-comment-row").each(function(index) {
+						$(this).addClass("gray-out");
+					});
+					//assign class to focus div
+					$(this).addClass('comment-hover');
+					$(this).removeClass('gray-out');
 
-				$('#popup-shoutout-submit').delay(3000).fadeIn('slow');
-				$('#popup-shoutout-submit').delay(3100).fadeOut('slow');
-				$('form#webform-client-form-84 input.webform-submit').toggle( "bounce", { times: 3 }, "slow" );
 
+
+				}
+
+			});
+
+			//$( ".shoutout-comment-row" ).each(function( index ) {
+			//	$(this).addClass("bounceIn animated").delay(2000).queue(function(next){
+			//		$(this).removeClass("bounceIn animated");
+			//		next();
+			//	});
+			//	//$(this).delay(5000).addClass('bounceIn animated');
+			//	//$(this).removeClass('bounceIn animated');
+            //
+			//});
+
+			$(".shoutout-comment-row").each(function(index) {
+				$(this).delay(90*index).fadeIn(300).addClass("bounceIn animated").queue(function(next){
+					$(this).removeClass("bounceIn animated");
+					next();
+				});
+			});
+			//gradient shine
+			//$('.shout-header2').gradientify({
+			//	gradients: [
+			//		{ start: [0,0,0], stop: [50,50,50] },
+			//		{ start: [15,15,15], stop: [0,0,0] },
+			//		{ start: [0,0,0], stop: [10,10,10] }
+			//	]
+			//});
+
+			//activate bootstrap tooltips
+			$(function () {
+				$("[rel='tooltip']").tooltip();
 			});
 
 
