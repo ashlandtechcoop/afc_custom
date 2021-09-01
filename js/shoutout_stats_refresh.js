91c98833c712fc5a2eaa179d4a073916b197bf11
phandolin <phandolin@gmail.com>
2021-09-01 12:42:50 -0400

    Updated js to fix shoutout issues after purpleair widgets

--- js/shoutout_stats_refresh.js
+++ js/shoutout_stats_refresh.js
@@ -2,27 +2,27 @@
     Drupal.behaviors.blockRefresh = {
         attach: function (context, settings) {
             //shout stats search icon button refresh the view if it's hidden
-            jQuery('#shout-search-icon img').once().click(function() {
-                if (jQuery('div.view-shoutout-comment-list div.view-filters').is(":hidden")) {
+            $('#shout-search-icon img').once().click(function() {
+                if ($('div.view-shoutout-comment-list div.view-filters').is(":hidden")) {
                 } else {
-                    jQuery.each(Drupal.views.instances, function (i, view) {
+                    $.each(Drupal.views.instances, function (i, view) {
                         var selector = '.view-dom-id-' + view.settings.view_dom_id;
                         if (view.settings.view_display_id == "block") {
-                            jQuery(selector).triggerHandler('RefreshView');
+                            $(selector).triggerHandler('RefreshView');
                         }
-                        jQuery(selector).unbind();
+                        $(selector).unbind();
                     });
                 }
 
             });
             //also refresh it when someone hits refresh icon
-            jQuery('.refresh-icon').once().click(function() {
-                jQuery.each(Drupal.views.instances, function (i, view) {
+            $('.refresh-icon').once().click(function() {
+                $.each(Drupal.views.instances, function (i, view) {
                     var selector = '.view-dom-id-' + view.settings.view_dom_id;
                     if (view.settings.view_display_id == "block") {
-                        jQuery(selector).triggerHandler('RefreshView');
+                        $(selector).triggerHandler('RefreshView');
                     }
-                    jQuery(selector).unbind();
+                    $(selector).unbind();
                     $('div.view-shoutout-comment-list div.view-filters').fadeOut('slow');
 
                 });
